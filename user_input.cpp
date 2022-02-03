dadebddb34ab0346b950b89c5c84e0685512821b
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-02-03 14:38:22 -0500

    added short program which highlights use of cin

---
+++ user_input.cpp
@@ -0,0 +1,12 @@
+//short file which shows how to use cin to get input from the keyboard
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string input;
+
+    cin >> input;
+    cout << "Your input: " + input << endl;
+    return 0;
+}
