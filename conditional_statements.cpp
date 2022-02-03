a09634bde17d97db6b28506127a0c4db44f978d7
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-02-03 15:15:53 -0500

    conditionals file added

---
+++ conditional_statements.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    //looks to be the same as C and Java
+    int a = 1, b = 1;
+
+    if (a == b){
+        cout << "a == b -> true" << endl;
+    }
+    else if (false)
+    {
+
+    }
+
+    return 0;
+}
--- user_input.cpp
+++ user_input.cpp
@@ -6,7 +6,13 @@ using namespace std;
 int main(){
     string input;
 
+    cout << "Enter a string: " << flush;
     cin >> input;
     cout << "Your input: " + input << endl;
+
+    //cin also works for ints
+    int int_input;
+    cin >> int_input;
+    cout << "Your int: " << int_input << endl;
     return 0;
 }
