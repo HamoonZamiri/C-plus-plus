60b342ba733cdd1f521394d005049bc79f81f6f7
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-02-03 14:30:47 -0500

    playing with cout and variables

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main(){
+    int num = 5;
+    string test = "test";
+
     cout << "Hello World" << endl;
+    cout << num << endl;
+    cout << "My favourite number: " << num << endl;
+    cout << "Favourite number * 2 = " << num * 2 << endl;
+    num++;
+    cout << "num++ check -> expected: 6, actual: " << num << endl;
+    num+=1;
+    cout << "num+=1 check -> expected: 7, actual: " << num << endl;
+    cout << "String test: " + test << endl;
+
     return 0;
 }
