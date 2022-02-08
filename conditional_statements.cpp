a86bbd21526b60ee3fa2ac26f5505c1946a903fc
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-02-08 15:23:06 -0500

    introduced file with arrays

--- conditional_statements.cpp
+++ conditional_statements.cpp
@@ -6,6 +6,8 @@ int main() {
 
     //looks to be the same as C and Java
     int a = 1, b = 1;
+    int arr[5] = {1, 2, 3, 4, 5}; //arrays are same as in C, vectors are dynamic arrays
+    int arr2[] = {1, 2, 3};
 
     if (a == b){
         cout << "a == b -> true" << endl;
@@ -14,6 +16,10 @@ int main() {
     {
 
     }
+    else{
+
+    }
+    cout << "arr[2] = " << arr2[2] << endl;
 
     return 0;
 }
